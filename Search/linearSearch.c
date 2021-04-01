130f26004e1127c278413a31238b6e1ac229a07a
0xe82de <0xe82de@gmail.com>
2021-04-01 23:26:30 +0900

    2021-04-01 linearSearch.c update

---
+++ Search/linearSearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define FALSE -1
+
+int LinearSearch(int array[], int len, int target) {
+	int index;
+
+	for (index = 0; index < len; index++) {
+		printf("#");
+		if (array[index] == target) {
+			return index;
+		}
+	}
+
+	return FALSE;
+}
+
+int main(void) {
+	int array[100] = { 1, 2, 3, 4, 5, 0, };
+	int target[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int idx;
+	clock_t start, finish;
+	float time;
+
+	for (int i = 0; i < (sizeof(target) / sizeof(int)); i++) {
+		start = clock();
+		idx = LinearSearch(array, sizeof(array)/sizeof(int), target[i]);
+		finish = clock();
+		time = (float)(finish - start) / CLOCKS_PER_SEC;
+	
+		printf("\n< Result of target %d >\n", target[i]);
+		if (idx == FALSE) {
+			printf("There are no target %d in the array.\n", target[i]);
+			printf("Run Time: %.5lf\n\n", time);
+		}
+		else {
+			printf("Index of target %d is %d.\n", target[i], idx);
+			printf("Run Time: %.5lf\n\n", time);
+		}
+	}
+
+	return 0;
+}
