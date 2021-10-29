db2e2394271d781a588999aaeacf2e495e27e395
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-29 23:21:27 +0900

    삽입 정렬

---
+++ Algorithm/Sort/InsertionSort.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * 삽입 정렬
+ */
+
+public class InsertionSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = {5, 4, 3, 2, 1};
+        final int N = arr.length;
+
+        bw.write("정렬 전 : " + Arrays.toString(arr) + "\n");
+        InsertionSort(arr);
+        bw.write("정렬 후 : " + Arrays.toString(arr));
+
+        // io close
+        bw.close();
+    }
+
+    private static void InsertionSort(int[] arr) {
+        final int SIZE = arr.length;
+        int temp, j;
+        for (int i = 1; i < SIZE; ++i) {
+            temp = arr[i];
+            j = i - 1;
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
