163052a834dea04750f9d1ed70e746bba8c37009
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-30 00:02:16 +0900

    거품 정렬 구현

---
+++ Algorithm/Sort/BubbleSort.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+/**
+ * 거품 정렬
+ */
+
+public class BubbleSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = {5, 4, 3, 2, 1};
+        final int N = arr.length;
+
+        bw.write("정렬 전 : " + Arrays.toString(arr) + "\n");
+        BubbleSort(arr);
+        bw.write("정렬 후 : " + Arrays.toString(arr));
+
+        // io close
+        bw.close();
+    }
+
+    private static void BubbleSort(int[] arr) {
+        final int SIZE = arr.length - 1;
+        int temp;
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE - i; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
