fc959f437fe9788bd3a35a2e65dc6003754afec5
김경석 <0xe82de@gmail.com>
2022-04-06 00:09:49 +0900

    Update: 퀵 정렬 추가

--- Algorithm/MySort.java
+++ Algorithm/MySort.java
@@ -15,6 +15,12 @@ public class MySort {
         System.out.println("[삽입 정렬] 정렬 전 => " + Arrays.toString(arr));
         insertionSort(arr);
         System.out.println("[삽입 정렬] 정렬 후 => " + Arrays.toString(arr));
+        System.out.println();
+
+        arr = createArr();
+        System.out.println("[퀵 정렬] 정렬 전 => " + Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("[퀵 정렬] 정렬 후 => " + Arrays.toString(arr));
     }
 
     static int[] createArr() {
@@ -52,5 +58,51 @@ public class MySort {
                 }
             }
         }
+    }
+
+    static void quickSort(int[] arr, int src, int dst) {
+        if (src >= dst) {
+            return;
+        }
+
+        int pivot = src;
+        int left = pivot + 1;
+        int right = dst;
+
+        while (left <= right) {
+            /**
+             * 피벗보다 큰 값이 나올때까지 left 증가
+             */
+            while (left <= right && arr[pivot] >= arr[left]) {
+                ++left;
+            }
+
+            /**
+             * 피벗보다 작은 값이 나올 때까지 right 감소
+             */
+            while (right >= left && arr[pivot] <= arr[right]) {
+                --right;
+            }
+
+            int temp = 0;
+            if (left > right) {
+                /**
+                 * 엇갈리면 피벗과 작은 데이터가 된 right를 교환
+                 */
+                temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            } else {
+                /**
+                 * left right 교환
+                 */
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+
+        quickSort(arr, src, right - 1);
+        quickSort(arr, right + 1, dst);
     }
 }
